076e20861e6b6953aefb158b0845b9e621158279
marschallpaul15-sudo <marschallpaul15@gmail.com>
2026-08-18 18:01:00 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE_NAME="fitplan-v1";
+const APP_SHELL=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{
+ if(e.request.method!=="GET")return;
+ e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).catch(()=>cached)));
+});
